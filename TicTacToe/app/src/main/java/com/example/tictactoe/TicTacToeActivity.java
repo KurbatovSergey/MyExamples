ed72350125ebb920e0f9d7cc0946c79f85da9317
Kurbatov <onreception@gmail.com>
2016-06-23 17:07:24 +0300

    Im starting develop TicTacToe for Android

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/TicTacToeActivity.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class TicTacToeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
